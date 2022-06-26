43bfb52238a9f3fd71fed22bbf05f907d9d95302
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-26 19:32:49 +0100

    Adde some routes

--- index.js
+++ index.js
@@ -23,11 +23,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // my routes
+
 app.get('/products', async(req, res) => {
 const products = await Product.find({})
     res.render('products/index', { products });
 })
 
+app.get('/products/:id', async(req, res) => {
+const {id} = req.params;
+const product = await Product.findById(id)
+res.render('products/show', {product});
+})
+
+
+
+
+//my server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
