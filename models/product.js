c2ac601721ee57ccf7a02fc79c84b99df2185220
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-26 18:23:00 +0100

    Hard coded some products

--- models/product.js
+++ models/product.js
@@ -13,6 +13,7 @@ const productSchema = new mongoose.Schema({
     },
     category: {
         type: String,
+        lower: true,
         enum: ['vegetable','tomatoe','orange', 'fruit']
     }
 })
--- seeds.js
+++ seeds.js
@@ -10,3 +10,62 @@ mongoose.connect('mongodb://localhost:27017/myalx', {useNewUrlParser: true})
     console.log("OH NO ERROR!!!")
     console.log(err)
 })
+
+// const p = new Product ({
+//     name: 'Cassava',
+//     price: 129,
+//      category: 'fruit'
+
+// })
+// p.save().then(p => {
+//     console.log(p)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
+
+const seedProducts = [
+    {
+        name: 'Curry',
+        price: 3.4,
+        category: 'vegetable'
+    },
+    {
+        name: 'Water Melon',
+        price: 12.4,
+        category: 'fruit'
+    },
+    {
+        name: 'Apples',
+        price: 14.5,
+        catergory: 'fruit'
+    },
+    {
+        name: 'Avocado',
+        price: 76.4,
+        category: 'fruit'
+    },
+    {
+        name: 'Damson',
+        price: 123.4,
+        category: 'fruit'
+    },
+    {
+        name: 'potato',
+        price: 564.6,
+        category: 'vegetable'
+    },
+    {
+        name: 'Brinjal',
+        price: 6753.6,
+        category: 'vegetable'
+    }
+]
+
+Product.insertMany(seedProducts)
+.then(res => {
+    console.log(res)
+})
+.catch(e => {
+    console.log(e)
+})
