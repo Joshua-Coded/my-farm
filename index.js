46ed492b712711b76e0c4a2f9fe2df363dc38f42
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-26 18:42:04 +0100

    Added ejs for serving my template

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // my routes
-app.get('/alx', (req, res) => {
-    res.send("Hello here!");
+app.get('/products', async(req, res) => {
+const products = await Product.find({})
+    res.render('products/index', { products });
 })
 
 app.listen(3000, () => {
